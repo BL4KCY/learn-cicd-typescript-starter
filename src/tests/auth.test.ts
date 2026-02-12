4820f1d6457e3c34092f50763e4a2ee6be267a94
BL4KCY <mostafafarssiui@gmail.com>
2026-02-12 23:24:57 +0100

    formating code and check formating  in  ci.yml

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,50 +1,42 @@
-import {expect, test} from 'vitest'
-import { getAPIKey } from './src/api/auth.js'
-import { IncomingHttpHeaders } from 'http';
+import { expect, test } from "vitest";
+import { getAPIKey } from "./src/api/auth.js";
+import { IncomingHttpHeaders } from "http";
 
-const apikey: string = 'Gty5PVEuGd73pIsstV0hqid2UOaoUAK7';
+const apikey: string = "Gty5PVEuGd73pIsstV0hqid2UOaoUAK7";
 
 // test empty header
-const emptyHeader: IncomingHttpHeaders = {
-};
-
+const emptyHeader: IncomingHttpHeaders = {};
 
 test("empty header", () => {
-	expect(getAPIKey(emptyHeader)).toBeNull()
-})
+  expect(getAPIKey(emptyHeader)).toBeNull();
+});
 
-
-// test valid header 
+// test valid header
 
 const validHeader: IncomingHttpHeaders = {
-	authorization: "ApiKey " + apikey
-}
+  authorization: "ApiKey " + apikey,
+};
 
 test("valid header", () => {
-	expect(getAPIKey(validHeader)).toBe(apikey)
-})
-
-
+  expect(getAPIKey(validHeader)).toBe(apikey);
+});
 
 // test different authentication scheme
 
-
 const diffAuthSchemeHeader: IncomingHttpHeaders = {
-	authorization: "Bearar " + apikey
-}
+  authorization: "Bearar " + apikey,
+};
 
 test("different authentication scheme", () => {
-	expect(getAPIKey(diffAuthSchemeHeader)).toBeNull();
-})
-
+  expect(getAPIKey(diffAuthSchemeHeader)).toBeNull();
+});
 
 // test different apiKey
 
-
 const diffApiKeyHeader: IncomingHttpHeaders = {
-	authorization: "Bearar invalidKey"
-}
+  authorization: "Bearar invalidKey",
+};
 
 test("different authentication scheme", () => {
-	expect(getAPIKey(diffAuthSchemeHeader)).toBeNull();
-}) 
+  expect(getAPIKey(diffAuthSchemeHeader)).toBeNull();
+});
